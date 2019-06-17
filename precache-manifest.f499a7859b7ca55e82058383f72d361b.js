33142a410d44f315daae854e8072b0bb68e82d41
jubeen12 <jubeen12@gmail.com>
2019-06-17 12:48:54 +0530

    Updates

---
+++ precache-manifest.f499a7859b7ca55e82058383f72d361b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e68c9af9bfc962256adaa490944292b",
+    "url": "/redux-colorshift/index.html"
+  },
+  {
+    "revision": "38e545f3d9d350595d52",
+    "url": "/redux-colorshift/static/js/2.9aca150c.chunk.js"
+  },
+  {
+    "revision": "2caf7cfcbf736a52efd5",
+    "url": "/redux-colorshift/static/js/main.113bc92c.chunk.js"
+  },
+  {
+    "revision": "b64997b681dd127e8284",
+    "url": "/redux-colorshift/static/js/runtime~main.44f88933.js"
+  }
+]);
